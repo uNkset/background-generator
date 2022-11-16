fb7d47480b79bdc48c8fd93b0dadfcda6b4972f2
Konstantin Arescenko <kostylo@windowslive.com>
2022-11-16 10:34:26 +0100

    Simple generator phase 1

---
+++ script.js
@@ -0,0 +1,16 @@
+var css = document.querySelector('h3');
+var c1 = document.getElementById('c1');
+var c2 = document.querySelector('#c2');
+var body = document.getElementById('bg-color');
+
+function setGradient() {
+    body.style.background = 'linear-gradient(to right, '
+    + c1.value + ', ' + c2.value + ')';
+
+    css.textContent = body.style.background + ';';
+}
+
+c1.addEventListener('input', setGradient);
+c2.addEventListener('input', setGradient);
+
+// textContent, innerHtml, textNode
